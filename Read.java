0200e994e73385802d4b30384a1bc4982aadb820
André Noronha <102299652+KamaNay@users.noreply.github.com>
2024-05-08 12:59:12 -0300

    Update Read.java

--- Read.java
+++ Read.java
@@ -1,13 +1,12 @@
-
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
 public class Read {
-    public static void read(String token, PrintWriter out, BufferedReader in) throws IOException {
+        public static void read(BufferedReader reader, PrintWriter out, BufferedReader in, String token) throws IOException {
         if (token == null) {
             System.out.println("Por favor, faça login antes de consultar as informações da conta.");
             return;
@@ -23,7 +22,7 @@ public class Read {
         if (responseJson.get("status").getAsString().equals("SUCCESS")) {
             JsonObject dataObject = responseJson.getAsJsonObject("data");
             if (dataObject != null) {
-                JsonElement passwordElement = dataObject.get("password");
+            	JsonElement passwordElement = dataObject.get("password");
                 JsonElement emailElement = dataObject.get("email");
                 JsonElement nameElement = dataObject.get("name");
                 if (emailElement != null) {
@@ -33,12 +32,11 @@ public class Read {
                     System.out.println("Nome: " + nameElement.getAsString());
                 }
                 if (passwordElement != null) {
-                    System.out.println("Senha: " + passwordElement.getAsString());
+                	System.out.println("Senha: " + passwordElement.getAsString());
                 }
             }
         } else {
-            System.out
-                    .println("Erro ao consultar as informações da conta: " + responseJson.get("status").getAsString());
+            System.out.println("Erro ao consultar as informações da conta: " + responseJson.get("status").getAsString());
         }
     }
 }
