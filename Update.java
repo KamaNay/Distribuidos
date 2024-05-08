4b6922906da9ab756186090a6de43362320736e2
André Noronha <102299652+KamaNay@users.noreply.github.com>
2024-05-08 13:00:51 -0300

    Update Update.java

--- Update.java
+++ Update.java
@@ -6,45 +6,31 @@ import com.google.gson.JsonObject;
 
 public class Update {
     public static void update(BufferedReader reader, PrintWriter out, BufferedReader in, String token) throws IOException {
-    	
-    	if (token == null || token.isEmpty()) {
-            System.out.println("Por favor, logue antes de atualizar a conta.");
+        if (token == null) {
+            System.out.println("Por favor, faça login antes de atualizar a conta.");
             return;
         }
-    	   	
-        System.out.println("Digite seu novo endereço de email:");
+
+        System.out.println("Digite o novo endereço de email:");
         String email = reader.readLine();
 
-        if (email == null) {
-            System.out.println("Inválido");
-            return;
-        }
-
-        System.out.println("Digite sua nova senha:");
+        System.out.println("Digite a nova senha:");
         String password = reader.readLine();
 
-        if (password == null) {
-            System.out.println("Inválido");
-            return;
-        }
-
-        System.out.println("Digite seu novo nome:");
+        System.out.println("Digite o novo nome:");
         String name = reader.readLine();
 
-        if (name == null) {
-            System.out.println("Inválido");
-            return;
-        }
-
-        JsonObject requestJson = Request.createRequest("UPDATE_ACCOUNT_CANDIDATE");
+        JsonObject requestJson = new JsonObject();
+        requestJson.addProperty("operation", "UPDATE_ACCOUNT_CANDIDATE");
+        requestJson.addProperty("token", token);
         JsonObject data = new JsonObject();
         data.addProperty("email", email);
         data.addProperty("password", password);
         data.addProperty("name", name);
-        data.addProperty("token", token);
         requestJson.add("data", data);
 
-        String jsonResponse = Request.sendRequest(requestJson, out, in);
+        out.println(requestJson.toString());
+        String jsonResponse = in.readLine();
         System.out.println(jsonResponse);
     }
 }
